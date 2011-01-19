a463d724532e16307b7b12413037fe8e5d304721
Richard W.M. Jones <rjones@redhat.com>
2011-01-19 18:28:33 +0000

    Add regression test for RHEL 5 Xen DomU HVM (aka full virtualization).

---
+++ tests/test-rhel5-xen-domU-hvm.sh
@@ -0,0 +1,35 @@
+# Test for RHEL 5 Xen DomU HVM (aka full virtualization).
+# Copyright (C) 2008-2011 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+root=tests/rhel5-xen-domU-hvm
+
+output="$(./virt-what --test-root=$root 2>&1)"
+expected="xen
+xen-hvm"
+
+if [ "$output" != "$expected" ]; then
+    echo "$0: test failed because output did not match expected"
+    echo "Expected output was:"
+    echo "----------------------------------------"
+    echo "$expected"
+    echo "----------------------------------------"
+    echo "But the actual output of the program was:"
+    echo "----------------------------------------"
+    echo "$output"
+    echo "----------------------------------------"
+    exit 1
+fi
